7212125fdd30718a075d50c5e845d946d35f3218
tungnguyen <tungnguyenx99@gmail.com>
2025-04-06 21:11:20 +0700

    fix eslint

--- src/app/footer/useFooter.ts
+++ src/app/footer/useFooter.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import instance from "@/lib/axios-interceptor";
 import { IFooterData } from "@/lib/types/IFooter";
 import { IMeta } from "@/lib/types/IPagination";
